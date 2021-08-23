4d93b4a95f2c9750b616ec154e78bead55c48ee5
Mark Statkus <mstatkus@lululemon.com>
2021-08-23 15:22:26 -0700

    adding shipping cus why not.

--- src/product.js
+++ src/product.js
@@ -1,9 +1,10 @@
 export class Product {
-  constructor({id, name, type, price, tax}) {
+  constructor({id, name, type, price, tax, shipping}) {
     this.id = id
     this.name = name
     this.type = type
     this.price = price
     this.tax = tax
+    this.shipping = shipping
   }
 }
